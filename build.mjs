9ea7e5e8909c17db9f92e1f999146dba5dfe1fae
essaq199 <essaq199@gmail.com>
2026-04-24 13:43:25 +0300

    fix: use CJS format to fix dynamic require issue

--- build.mjs
+++ build.mjs
@@ -2,8 +2,8 @@ import { build } from "esbuild";
 await build({
   entryPoints: ["src/index.ts"],
   bundle: true,
-  outfile: "dist/index.mjs",
-  format: "esm",
+  outfile: "dist/index.cjs",
+  format: "cjs",
   platform: "node",
   target: "node22",
   sourcemap: true,
